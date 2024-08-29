27a1af1386f91e96c32e14440c765bbf2e43e2c9
K1 <k1fullstackk@gmail.com>
2024-08-29 11:29:32 +0300

    index html

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,14 +1,6 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
-import java.util.Set;
 
 /** Created by jt on 7/18/18. */
-public interface PetService {
-
-  Pet findById(Long id);
-
-  Pet save(Pet pet);
-
-  Set<Pet> findAll();
-}
+public interface PetService extends CrudService<Pet, Long> {}
